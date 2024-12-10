909b953138bed1eec0214f18d47d246912500e33
maria sidorova <sidmashav@icloud.com>
2024-12-10 16:23:49 +0000

    RegisterPage

--- Ash_Moon/front-end/src/App.jsx
+++ Ash_Moon/front-end/src/App.jsx
@@ -5,6 +5,7 @@ import ProductPage from "./pages/ProductPage";
 import ProductsGallery from "./components/ProductsGallery";
 import LoginPage from "./pages/LoginPage";
 import NewProduct from "./components/NewProduct";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <>
@@ -17,7 +18,7 @@ function App() {
 
           <Route path="/new" element={<ProductsGallery category={"new"} />} />
           <Route path="/sale" element={<ProductsGallery category={"sale"} />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<RegisterPage />} />
           <Route path="/addNew" element={<NewProduct />} />
         </Routes>
       </BrowserRouter>
