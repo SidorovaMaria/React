4afbb1e616b292bb4afe3b57525af24396e06564
maria sidorova <sidmashav@icloud.com>
2025-01-14 10:26:05 +0200

    Initial backend Ash&Moon

---
+++ Ash-Moon/backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser);
+
+//TODO AUTHENTICATION ROUTE
+// app.use("/ash&moon/auth", authRoutes);
+//TODO PRODUCT ROUTE
+// app.use("/ash&moon/products", productRoutes);
+
+app.listen(PORT, () => {
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
+});
